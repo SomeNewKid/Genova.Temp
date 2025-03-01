32b260aa74322e7414ad75f86903a76a37e55141
SomeNewKid <SomeNewKid@hotmail.com>
2025-03-02 00:57:19 +0800

    (1) Add basic language switcher.

--- ResponseModifiers/HtmlResponseModifier.cs
+++ ResponseModifiers/HtmlResponseModifier.cs
@@ -17,6 +17,43 @@ public class HtmlResponseModifier : TextResponseModifier
         {
             message = $"<p>Current culture: {ResponseContext.CurrentPageCulture.DisplayName}</p>";
         }
-        return content.Replace("</body>", $"<p>Modified by Middleware</p>{message}</body>");
+        string modifiedContent = content.Replace("</body>", $"<p>Modified by Middleware</p>{message}</body>");
+
+        // Split the modified content into head and body parts
+        string splitAt = "</nav>";
+        int headEndIndex = modifiedContent.IndexOf(splitAt) + splitAt.Length;
+        string modifiedHead = modifiedContent.Substring(0, headEndIndex);
+        string modifiedBody = modifiedContent.Substring(headEndIndex);
+
+        modifiedBody = RemoveLinkSlug(modifiedBody, "zh");
+        modifiedBody = RemoveLinkSlug(modifiedBody, "zh-hk");
+        modifiedBody = RemoveLinkSlug(modifiedBody, "ar");
+
+        if (ResponseContext != null &&
+            ResponseContext.CurrentPageCulture != null)
+        {
+            string cultureSlug = ResponseContext.CurrentPageCulture.Name;
+            if (cultureSlug != "en")
+            {
+                modifiedBody = PrependLinkSlug(modifiedBody, cultureSlug.ToLowerInvariant());
+                modifiedHead = modifiedHead.Replace(" lang=\"en\"", $" lang=\"{cultureSlug}\"");
+                if (cultureSlug == "ar")
+                {
+                    modifiedHead = modifiedHead.Replace(" lang=\"ar\"", " lang=\"ar\" dir=\"rtl\"");
+                }
+            }
+        }
+        return modifiedHead + modifiedBody;
+    }
+
+    private string PrependLinkSlug(string content, string slug)
+    {
+        return content.Replace(" href=\"", " href=\"/" + slug);
+    }
+
+    private string RemoveLinkSlug(string content, string slug)
+    {
+        content = content.Replace(" href=\"/" + slug, " href=\"/");
+        return content.Replace(" href=\"//", " href=\"/");
     }
 }
