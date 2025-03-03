3b77681f917a8957532f89300d6dc1c90ea58f84
SomeNewKid <SomeNewKid@hotmail.com>
2025-03-03 12:05:35 +0800

    (1) Separate _Layout.cshtml into separate partial view files.

--- ResponseModifiers/HtmlResponseModifier.cs
+++ ResponseModifiers/HtmlResponseModifier.cs
@@ -6,6 +6,15 @@ namespace Genova.Temp.ResponseModifiers;
 
 public class HtmlResponseModifier : TextResponseModifier
 {
+    // ASP.NET Core and its tag helpers often force changes to HTML element attributes,
+    // such as forcably changing lang="" forcibly inserting dir="" attributes.
+    // To stop this, the Razor files may prepend the following prefix to attributes,
+    // so that the rendered attribute is MODIFY_ATTRIBUTE_PREFIX_lang="".
+    // After ASP.NET has finished rendered the HTML, we need to remove
+    // this attribute prefix.
+    // Hacky, sure, but ChatGPT and I could not figure out any other way of
+    // suppressing ASP.NET's insistence on changing some attribute values.
+
     public const string MODIFY_ATTRIBUTE_PREFIX = "MODIFY_ATTRIBUTE_PREFIX_";
 
     public HtmlResponseModifier(ResponseContext responseContext)
@@ -23,46 +32,8 @@ public class HtmlResponseModifier : TextResponseModifier
         }
         string modifiedContent = content; // content.Replace("</body>", $"<p>Modified by Middleware</p>{message}</body>");
 
-        // Split the modified content into head and body parts
-        string splitAt = "</nav>";
-        int headEndIndex = modifiedContent.IndexOf(splitAt) + splitAt.Length;
-        string modifiedHead = modifiedContent.Substring(0, headEndIndex);
-        string modifiedBody = modifiedContent.Substring(headEndIndex);
+        modifiedContent = modifiedContent.Replace(MODIFY_ATTRIBUTE_PREFIX, "").Trim();
 
-        modifiedBody = RemoveLinkSlug(modifiedBody, "en");
-        modifiedBody = RemoveLinkSlug(modifiedBody, "zh");
-        modifiedBody = RemoveLinkSlug(modifiedBody, "zh-hk");
-        modifiedBody = RemoveLinkSlug(modifiedBody, "ar");
-
-        if (ResponseContext != null &&
-            ResponseContext.CurrentPageCulture != null)
-        {
-            string cultureSlug = ResponseContext.CurrentPageCulture.Name;
-            string defaultCultureSlug = CultureInfo.CurrentUICulture.Name.ToLowerInvariant();
-            string dir = ResponseContext.CurrentPageCulture.TextInfo.IsRightToLeft ? "dir=\"rtl\"" : "dir=\"ltr\"";
-
-            if (!cultureSlug.Equals(defaultCultureSlug, StringComparison.OrdinalIgnoreCase))
-            {
-                modifiedBody = PrependLinkSlug(modifiedBody, cultureSlug.ToLowerInvariant());
-            }
-
-            // Use regular expression to replace the lang attribute and inject the dir attribute
-            modifiedHead = Regex.Replace(modifiedHead, @"\s+lang=""[^""]*""", $" lang=\"{cultureSlug}\" {dir}");
-        }
-
-        modifiedHead = modifiedHead.Replace(MODIFY_ATTRIBUTE_PREFIX, ""); // the language switcher is considered part of the "head", so that its links are not changed
-
-        return modifiedHead.TrimStart() + modifiedBody.TrimEnd();
-    }
-
-    private string PrependLinkSlug(string content, string slug)
-    {
-        return content.Replace(" href=\"", " href=\"/" + slug);
-    }
-
-    private string RemoveLinkSlug(string content, string slug)
-    {
-        content = content.Replace(" href=\"/" + slug, " href=\"/");
-        return content.Replace(" href=\"//", " href=\"/");
+        return modifiedContent;
     }
 }
